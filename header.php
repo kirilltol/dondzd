b4fc18b7b1cdcbe19ba26076a249c24a26ae20dd
kirilltol <64205174+kirilltol@users.noreply.github.com>
2020-09-10 15:54:21 +0300

    Add files via upload
    upload

---
+++ header.php
@@ -0,0 +1,14 @@
+
+    <header>
+        <nav>
+            <h1>Государственное предприятие<br>
+                “Донецкая железная дорога”</h1>
+            <div class="menu">
+                <div class="effect-nav"><a href="index.php">Главная</a></div>
+                <div class="effect-nav"><a href="about.php">О нас</a></div>
+                <div class="effect-nav"><a href="pass.php">Пассажирам</a></div>
+                <div class="effect-nav"><a href="students.php">Обучающимся</a></div>
+            </div>
+        </nav>
+    </header>
+   
